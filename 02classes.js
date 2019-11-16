a7ff39c0ad309a10ab4d5c7dfafae786707b655d
Dean McHugh <dean.mchugh1988@gmail.com>
2019-11-16 10:42:26 +1000

    modules

---
+++ 02classes.js
@@ -0,0 +1,106 @@
+//standard class. data at the top and functions at the bottom
+class Page {
+    constructor(text) {
+        this.text = text;
+    }
+
+    print() {
+        console.log(this.text);
+    }
+}
+
+class Notebook {
+    constructor() {
+        this.pages = [];
+    }
+
+    addPage(text) {
+        var page = new Page(text);
+        this.pages.push(page);
+    }
+
+    print() {
+        for (let page of this.pages) {
+            page.print();
+        }
+    }
+}
+
+var mathNotes = new Notebook();
+mathNotes.addPage("Arithmetic: + - * / ...");
+mathNotes.addPage("Trigonometry: sin cos tan ...");
+
+mathNotes.print();
+
+//class inheritance defines a set of common behavior
+class Publication {
+    constructor(title,author,pubDate) {
+        this.title = title;
+        this.author = author;
+        this.pubDate = pubDate;
+    }
+
+    print() {
+        console.log(`
+            Title: ${this.title}
+            By: ${this.author}
+            ${this.pubDate}
+        `);
+    }
+}
+
+// book extents publication 
+class Book extends Publication {
+    constructor(bookDetails) {
+        super(
+            bookDetails.title,
+            bookDetails.author,
+            bookDetails.publishedOn
+        );
+        this.publisher = bookDetails.publisher;
+        this.ISBN = bookDetails.ISBN;
+    }
+
+    print() {
+        super.print();
+        console.log(`
+            Published By: ${this.publisher}
+            ISBN: ${this.ISBN}
+        `);
+    }
+}
+
+//blog post exents publication
+class BlogPost extends Publication {
+    constructor(title,author,pubDate,URL) {
+        super(title,author,pubDate);
+        this.URL = URL;
+    }
+
+    print() {
+        super.print();
+        console.log(this.URL);
+    }
+}
+
+//using the sbu or child classes
+var YDKJS = new Book({
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    publishedOn: "June 2014",
+    publisher: "O'reilly",
+    ISBN: "123456-789"
+});
+
+YDKJS.print();
+
+var forAgainstLet = new BlogPost(
+    "For and against let",
+    "Kyle Simpson",
+    "October 27, 2014",
+    "https://davidwalsh.name/for-and-against-let"
+);
+
+forAgainstLet.print();
+
+
---
+++ 04es6Modules/main.js
@@ -0,0 +1,10 @@
+import { create as createBlogPost } from "blogpost.js";
+
+var forAgainstLet = createBlogPost(
+    "For and against let",
+    "Kyle Simpson",
+    "October 27, 2014",
+    "https://davidwalsh.name/for-and-against-let"
+);
+
+forAgainstLet.print();
