e806a796abda721643d455de251a25f1f7296262
Dean McHugh <dean.mchugh1988@gmail.com>
2019-11-25 19:38:16 +1000

    scope

---
+++ scopeAndClosure/scope.js
@@ -0,0 +1,16 @@
+
+//shadowing
+var studentName = "Suzy";
+
+function printStudent(studentName) {
+    studentName = studentName.toUpperCase();
+    console.log(studentName);
+}
+
+printStudent("Frank");
+// FRANK
+
+printStudent(studentName);
+// SUZY
+
+console.log(studentName);
